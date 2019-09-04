c645b173a1b7ad489d7be9c35e7bedc82cf42711
Indyka Saidi <indyka@ultravoucher.co.id>
2019-09-04 21:46:01 +0700

    fixing error throw each interval

--- app.js
+++ app.js
@@ -5,29 +5,33 @@ const { upsertActivities, getReadyToPublishActivity, getTrackCount } = require('
 const { sendTweet } = require('./modules/twitter');
 
 const getNowPlaying = async () => {
-	console.log(`get spotify's now playing`);
-	const activities = await getActivities();
-
-	await upsertActivities(activities);
+	try {
+		console.log(`get spotify's now playing`);
+		const activities = await getActivities();
+		await upsertActivities(activities);
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
 };
 
 const publishToTwitter = async () => {
-	console.log('publish to twitter');
-	const activity = await getReadyToPublishActivity();
-
-	if (activity) {
-		await activity.update({ is_published: true });
-		if (activity.member) await sendTweet(activity, await getTrackCount(activity.track_uri, activity.spotify_uri));
+	try {
+		console.log('publish to twitter');
+		const activity = await getReadyToPublishActivity();
+		if (activity) {
+			await activity.update({ is_published: true });
+			if (activity.member) await sendTweet(activity, await getTrackCount(activity.track_uri, activity.spotify_uri));
+		}	
+	} catch (error) {
+		console.log(error);
+		throw false
 	}
 };
 
 const start = async () => {
-	try {
-		await getNowPlaying();
-		await publishToTwitter();
-	} catch (error) {
-		console.log(error);
-	}
+	await getNowPlaying();
+	await publishToTwitter();
 }
 
 start();
