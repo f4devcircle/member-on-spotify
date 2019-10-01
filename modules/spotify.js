8a3907393e17aacce907ffca29350f2e7edff8ea
Indyka Saidi <indyka@ultravoucher.co.id>
2019-10-02 04:07:11 +0700

    update auth

--- modules/spotify.js
+++ modules/spotify.js
@@ -6,22 +6,21 @@ const authEndpoint = process.env.SPOTIFY_AUTH_URL;
 
 const headerRequest = JSON.parse(fs.readFileSync('spotify-auth.json', 'utf8'));
 
-const headers = (token) => {
-  return {
-    "Authorization": "Bearer " + token,
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en",
-    "User-Agent": "Spotify/1.0.93 Win32/Windows 10 (10.0.17134; x64)",
-    "Connection": "keep-alive",
-    "Host": "spclient.wg.spotify.com"
-  };
-}
+const headers = (token) => ({
+	Authorization: `Bearer ${token}`,
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'en',
+	'User-Agent': 'Spotify/1.0.93 Win32/Windows 10 (10.0.17134; x64)',
+	Connection: 'keep-alive',
+	Host: 'spclient.wg.spotify.com',
+});
 
 const auth = async () => {
 	const response = await axios.get(authEndpoint, {
 		headers: headerRequest,
 	});
-	return response.headers['set-cookie'][2].split(';')[0].replace('wp_access_token=', '');
+
+	return response.data.split('accessToken":"')[1].split('","accessTokenExpirationTimestampMs"')[0];
 };
 
 const getActivities = async () => {
